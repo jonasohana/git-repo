9e30e84effb0d348910b753b4b9ebe2dda7c4b79
jonasohana <pauljonas@yahoo.com>
2017-11-14 14:02:42 -1000

    add

---
+++ htdocs/IntroPHP/testnewdir.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $diameter = 10;
+    $radius = ($diameter*.50);
+    $pie = 3.14159265359;
+    $circlearea = $pie * ($radius * $radius);
+    echo "a circle diameter $diameter and a radius of $radius has an area of $circlearea";
+
+
+    ?>
+</body>
+</html>
