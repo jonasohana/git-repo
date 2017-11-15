89840ab86523ecd710a6fd2b587837334d390d16
jonasohana <pauljonas@yahoo.com>
2017-11-14 14:07:44 -1000

    x

--- htdocs/IntroPHP/testnewdir.php
+++ htdocs/IntroPHP/testnewdir.php
@@ -12,7 +12,7 @@
     $radius = ($diameter*.50);
     $pie = 3.14159265359;
     $circlearea = $pie * ($radius * $radius);
-    echo "a circle diameter $diameter and a radius of $radius has an area of $circlearea";
+    echo "a circle <b>diameter $diameter</b> and a radius of $radius has an area of $circlearea";
 
 
     ?>
