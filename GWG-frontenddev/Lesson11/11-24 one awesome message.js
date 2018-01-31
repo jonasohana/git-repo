73c843690e428c03fee839d0ab62ceb89be6b1b9
Paul Jonas <pauljonas@yahoo.com>
2018-01-31 08:38:44 -1000

    x

--- GWG-frontenddev/Lesson11/11-24 one awesome message.js
+++ GWG-frontenddev/Lesson11/11-24 one awesome message.js
@@ -15,6 +15,21 @@
  * - Using the above as an example, firstName would have been assigned to
  *   "Julia", interest to "cats", and hobby to "play video games".
  * - Be sure to include spaces and periods where necessary!
+ * "Hi, my name is Julia. I love cats. In my spare time, I like to play video games."
  */
 
-// Add your code here
+ /* baseline */
+var firstName = "Julia";
+var interest = "cats";
+var hobby = "play video games";
+var awesomeMessage = "Hi, my name is "+ firstName + ". I love "+ interest + ". In my spare time, I like to "+ hobby + ".";
+
+console.log(awesomeMessage);
+
+// submitted 
+ var firstName = "Pablo";
+ var interest = "my dog, Einstein";
+ var hobby = "walk my dog";
+ var awesomeMessage = "Hi, my name is "+ firstName + ". I love "+ interest + ". In my spare time, I like to "+ hobby + ".";
+ 
+ console.log(awesomeMessage);
