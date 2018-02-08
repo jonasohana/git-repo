97bcebc2ff827d5cc39d638f216b736aab8b43d8
Paul Jonas <pauljonas@yahoo.com>
2018-02-08 11:01:38 -1000

    x

--- GWG-frontenddev/Lesson15/15-02 array test.js
+++ GWG-frontenddev/Lesson15/15-02 array test.js
@@ -1,3 +1,7 @@
-const arraysInArrays = [[1, 2, 3], ["Julia", "James"], [true, false, true, false]];
+const arraysInArrays = [
+    [1, 2, 3], 
+    ["Julia", "James"], 
+    [true, false, true, false]
+];
 
-for (const i=0;i<=length.arraysInArrays[])
+for (const i=0;i<=length.arraysInArrays[];i++ );
