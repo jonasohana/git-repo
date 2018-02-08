ef67d5eef19bcf4e93577c61b008bc70310d352b
Paul Jonas <pauljonas@yahoo.com>
2018-02-08 11:55:34 -1000

    x

--- GWG-frontenddev/Lesson13/13-13 fix the error 2.js
+++ GWG-frontenddev/Lesson13/13-13 fix the error 2.js
@@ -8,6 +8,32 @@ for (var k = 0 k < 200 k++) {
     console.log(k);
 }
 */
-for (var k = 0;k < 200;k++) {
+// does not follow Udacity Guidelines
+console.log("var");
+for (var k = 0;k < 5;k++) {
     console.log(k);
+}
+
+
+// this works
+console.log("let");
+for (let m = 0;m < 5;m++) {
+    console.log(m);
+}
+
+// no declaration
+console.log("no declaration");
+for (n = 0;n < 5;n++) {
+    console.log(n);
+}
+
+
+// Error 
+/*
+VM299:1 Uncaught TypeError: Assignment to constant variable.
+    at <anonymous>:1:27
+*/
+console.log("const - error");
+for (const l = 0;l < 5;l++) {
+    console.log(l);
 }
