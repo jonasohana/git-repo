de7d5753477b5df8bc8f8c537cadfc740175c97d
Paul Jonas <pauljonas@yahoo.com>
2018-02-07 14:58:18 -1000

    x

---
+++ GWG-frontenddev/Lesson16/16-05 literals text.js
@@ -0,0 +1,11 @@
+var sister = {
+    name: "Sarah", // attribute(s)
+    'age': 23, // attribute(s)
+    parents: [ "alice", "andy" ], // attribute(s)
+    siblings: ["julia"], // attribute(s)
+    favoriteColor: "purple", // attribute(s)
+    pets: true, // attribute(s)
+    paintPicture: function() { return "Sarah paints!"; } //method
+  };
+
+  
