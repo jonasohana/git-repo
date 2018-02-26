4da9a00e762cd3dd2fcc8b3fc1623ea660ea5643
Paul Jonas <pauljonas@yahoo.com>
2018-02-26 11:26:00 -1000

    x

---
+++ GWG-frontenddev/Lesson18/18-09 quiz - select class.js
@@ -0,0 +1,11 @@
+/*
+For this quiz, use a jQuery class selector to grab all of the elements of class 'article-item' on the page!
+*/
+
+// don't change this variable!
+var articleItems;
+
+articleItems = $('.article-item');
+
+// there is 1 article 
+// there are 4 article items
