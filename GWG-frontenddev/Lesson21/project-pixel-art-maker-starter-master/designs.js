19894f590599e474d83eb7de32a9c8d2d8c921d9
Paul Jonas <pauljonas@yahoo.com>
2018-03-26 15:09:59 -1000

    x

--- GWG-frontenddev/Lesson21/project-pixel-art-maker-starter-master/designs.js
+++ GWG-frontenddev/Lesson21/project-pixel-art-maker-starter-master/designs.js
@@ -1,91 +1,37 @@
 // Select color input
+var colorInput = $('#colorPicker');
 // Select size input
+var inputWidth = $('#input_width');
+var inputHeight = $('#input_height');
+// Select table
+var table = document.getElementById('pixel_canvas');
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}
-
-
-function generate_table() {
-
-    // const inputHeight = $('inputHeight');
-    // const inputWeight = $('inputWeight');
-
-      // get the reference for the body
-      var body = document.getElementsByTagName("body")[0];
-     
-      // creates a <table> element and a <tbody> element
-      var tbl = document.createElement("table");
-      var tblBody = document.createElement("tbody");
-     
-      // creating all cells
-      for (var i = 0; i < 10; i++) {
-        // creates a table row
-        var row = document.createElement("tr");
-     
-        for (var j = 0; j < 10; j++) {
-          // Create a <td> element and a text node, make the text
-          // node the contents of the <td>, and put the <td> at
-          // the end of the table row
-          var cell = document.createElement("td");
-        // var cellText = document.createTextNode("cell in row "+i+", column "+j);
-          var cellText = document.createTextNode(i+","+j);
-          cell.appendChild(cellText);
-          row.appendChild(cell);
-        // PJ 3/18 add id per cell 
-
-        }
-     
-        // add the row to the end of the table body
-        tblBody.appendChild(row);
-      }
-     
-      // put the <tbody> in the <table>
-      tbl.appendChild(tblBody);
-      // appends <table> into <body>
-      body.appendChild(tbl);
-      // sets the border attribute of tbl to 2;
-      tbl.setAttribute("border", "2");
-
-    // add click listener after created 
-    // $( "td" ).click(function() {
-    //     console.log( "click() called " );
-    // });
-
-    // $( 'td' ).on( 'click', function( evt ) {
-    //     evt.preventDefault();
-    //     console.log( 'You clicked a link!  on event:' + event.type + 
-    //     ' at X:' +  event.pageX + ' Y:' +  event.pageY
-    //     );
-    //     console.log("Cell index is: " + (this).cellIndex);
-    colorPicker.addEventListener("input", updateFirst, false);
-    colorPicker.addEventListener("change", watchColorPicker, false);
-    
-    function watchColorPicker(event) {
-      document.querySelectorAll("p").forEach(function(p) {
-        p.style.color = event.target.value;
-      });
+// $('#sizePicker').on('submit', function makeGrid() {
+//     // Remove all children of the old table
+//     while (table.firstChild)
+//         table.removeChild(table.firstChild);
+function makeGrid () {
+    // Add cells to table
+    for (var i = 0; i < inputHeight.val(); i++) {
+        var row = document.createElement('tr');
+        for (var j = 0; j < inputWidth.val(); j++) {
+            var cell = document.createElement('td');
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
     }
-    
-        $("td").hover(
-            function() {
-            //   $(this).css('background-color', color);
-            //   $(this).css('color', color);
-              $(this).css('background-color', '#ff0000');
-              $(this).css('color', '#000000');
-            }
-        );
 
-        // $( 'td' ).on( 'click', function( evt ) {
-        //     evt.preventDefault();
-        //     console.log( 'You clicked a box at X:' +  event.pageX + ' Y:' +  event.pageY);
-        //     $(this).css('background-color', '#ff0000');
-        //     $(this).css('color', '#000000');
-        // });
-    // });
+    // Don't reload the page
+//     return false;
+// });
+};
 
-    }
+// When a cell is clicked, call colorCell
+$('#pixel_canvas').on('click', 'td', function colorCell(evt) {
+    var background = (evt.target).style.backgroundColor;
+    var isWhiteBackground = !background || background === "white" || background === "rgb(255, 255, 255)" ||
+        background === "#fff" || background ==="#fffff";
+    var color = isWhiteBackground ? colorInput.val() : "white";
+    $(evt.target).css("background-color", color);
+});
--- GWG-frontenddev/Lesson21/project-pixel-art-maker-starter-master/table.js
+++ GWG-frontenddev/Lesson21/project-pixel-art-maker-starter-master/table.js
@@ -1,40 +1,12 @@
 function generate_table() {
-      // get the reference for the body
-      var body = document.getElementsByTagName("body")[0];
-     
-      // creates a <table> element and a <tbody> element
-      var tbl = document.createElement("table");
-      var tblBody = document.createElement("tbody");
-     
-      // creating all cells
-      for (var i = 0; i < 10; i++) {
-        // creates a table row
-        var row = document.createElement("tr");
-     
-        for (var j = 0; j < 10; j++) {
-          // Create a <td> element and a text node, make the text
-          // node the contents of the <td>, and put the <td> at
-          // the end of the table row
-          var cell = document.createElement("td");
-        // var cellText = document.createTextNode("cell in row "+i+", column "+j);
-          var cellText = document.createTextNode(i+","+j);
-          cell.appendChild(cellText);
-          row.appendChild(cell);
-        // PJ 3/18 add id per cell 
-
-        }
-     
-        // add the row to the end of the table body
-        tblBody.appendChild(row);
-      }
-     
-      // put the <tbody> in the <table>
-      tbl.appendChild(tblBody);
-      // appends <table> into <body>
-      body.appendChild(tbl);
-      // sets the border attribute of tbl to 2;
-      tbl.setAttribute("border", "2");
-
+        for (var i = 0; i < inputHeight.val(); i++) {
+            var row = document.createElement('tr');
+            for (var j = 0; j < inputWidth.val(); j++) {
+                var cell = document.createElement('td');
+                row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
     // add click listener after created 
     // $( "td" ).click(function() {
     //     console.log( "click() called " );
@@ -54,8 +26,14 @@ function generate_table() {
         p.style.color = event.target.value;
       });
     }
-    
-        $("td").hover(
+//     $('#input').on('change', function() {
+//         var val;
+//         val = $('#input').val(); // read input val 
+//         $('.articles').children('h1').text(val); // set val into h1
+//    }); 
+
+        $("td").on('click', 
+        // $("td").hover(
             function() {
             //   $(this).css('background-color', color);
             //   $(this).css('color', color);
