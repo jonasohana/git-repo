d7d883ca40c42285334932a485b37529f391909b
Paul Jonas <pauljonas@yahoo.com>
2018-03-28 15:59:28 -1000

    x

--- GWG-frontenddev/Lesson21/project-pixel-art-maker-starter-master/designElementID.js
+++ GWG-frontenddev/Lesson21/project-pixel-art-maker-starter-master/designElementID.js
@@ -36,35 +36,38 @@ console.log(inputWidth.value);
 //                 alert(this.id);},
 //                 false);
 // }
-// function sayHello () {
-//     console.log('hello');
+
 
 // function loaded() {
     document.getElementById("sizePicker").addEventListener("submit", 
         function(evt) {
-            evt.preventDefault();
+            // unless explicitly handled, prevent default event handling 
+            evt.preventDefault();  
+
             // alert('hello alert');
             console.log('hello');
+            makeGrid ()
             }
     ,false);
 
-    // function makeGrid () {
-    //     // reset/clear  table
-    //     table.innerHTML = "";
+    function makeGrid () {
+        // reset/clear  table
+        table.innerHTML = "";
 
-    //     // build a table using Node 'tr' and 'td'
-    //     for (h=0; h<inputHeight.value; h++) {
-    //         // h = height 
-    //         var row = document.createElement('tr'); // create Node 'tr' table row
-    //         for (w=0; w<inputWidth.value;  w++); {
-    //             // w = width 
-    //             var cell = document.createElement('td'); // create Node 'Td' - table cell 
-    //             row.appendChild(cell); // append cell(s) within a row 
-    //         }
-    //         document.getElementById('pixelCanvas').appendChild(row); // append the row and cell(s) to table
-    //     }
-    //     console.log(table.innerHTML);
-    // } // makeGrid()
+        // build a table using Node 'tr' and 'td'
+        for (h=0; h<inputHeight.value; h++) {
+            // h = height 
+            var row = document.createElement('tr'); // create Node 'tr' table row
+            row.appendChild(row); // row ++
+            for (w=0; w<inputWidth.value;  w++); {
+                // w = width 
+                var cell = document.createElement('td'); // create Node 'Td' - table cell 
+                row.appendChild(cell); // append cell(s) within a row 
+            }
+            document.getElementById('pixelCanvas').appendChild(row); // append the row and cell(s) to table
+        }
+        console.log(table.innerHTML);
+    } // makeGrid()
 
 // }
 // window.addEventListener("load", loaded, false);
